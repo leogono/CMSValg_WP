f30257d61d521d71c842c30d40cbdf60bc7055c1
Leo Gono <hello@wpmeds.com>
2013-09-30 22:54:17 +0800

    added ul of cms system for drop down.

---
+++ templates/list-cmssimple.php
@@ -0,0 +1,14 @@
+<?php 
+$taxonomy = 'cms_system';
+$tax_terms = get_terms($taxonomy);
+
+?>
+<ul class="dropdown-menu" role="menu">
+<?php
+foreach ($tax_terms as $tax_term) {
+  ?>
+    <li><a href="<?php echo esc_attr(get_term_link($tax_term, $taxonomy)); ?>"><?php echo $tax_term->name; ?></a></li>
+  <?php
+  }
+?>
+</ul>
