25e9d403997e98c7294ed033b9fab15ce983078b
Leo Gono <hello@wpmeds.com>
2013-10-01 23:36:11 +0800

    added ajax-loop on knowledge template

--- taxonomy-knowledge.php
+++ taxonomy-knowledge.php
@@ -8,14 +8,23 @@
     <?php get_search_form(); ?>
   <?php endif; ?>
 
-  <div id="cms-rel-posts">
+  <!-- <div id="cms-rel-posts">
     <div class="row">
       <?php while (have_posts()) : the_post(); ?>
         <?php get_template_part('templates/content', 'taxonomy'); ?>
       <?php endwhile; ?>
     </div>
-  </div>
+  </div> -->
+  
+  <?php
+  $term = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy'));
+  ?>
 
+  <div id="cms-rel-posts">
+      <div class="row" data-path="<?php echo get_stylesheet_directory_uri(); ?>/wp-content/themes/cmsvalg-wp/" data-post-type="post" data-category="" data-taxonomy="knowledge" data-tag="<?php echo $term->slug; ?>" data-display-posts="4" data-button-text="Se alle artikler om <?php echo $term->name; ?>">
+      <!-- Load Ajax Posts Here -->
+      </div>
+  </div>
 
   <?php if ($wp_query->max_num_pages > 1) : ?>
     <nav class="post-nav">
