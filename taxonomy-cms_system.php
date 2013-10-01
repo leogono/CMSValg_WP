26e2d9aac70ca842a8d6bb7aadf115f22d91badf
Leo Gono <hello@wpmeds.com>
2013-10-01 17:07:32 +0800

    integrated ajaxloop

--- taxonomy-cms_system.php
+++ taxonomy-cms_system.php
@@ -8,23 +8,32 @@
     <?php get_search_form(); ?>
   <?php endif; ?>
 
-  <div id="cms-rel-posts">
+  <!-- <div id="cms-rel-posts">
     <div class="row">
       <?php $i = 0; while (have_posts() && $i < 4) : the_post(); ?>
         <?php get_template_part('templates/content', 'taxonomy'); ?>
       <?php $i++; endwhile; ?>
     </div>
+  </div> -->
+
+  <?php
+  $term = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy'));
+  ?>
+
+  <div id="ajax-load-more">
+      <div class="row" data-path="<?php echo get_stylesheet_directory_uri(); ?>/wp-content/themes/cmsvalg-wp/" data-post-type="post" data-category="" data-taxonomy="cms_system" data-tag="<?php echo $term->slug; ?>" data-display-posts="4" data-button-text="Load More">
+      <!-- Load Ajax Posts Here -->
+      </div>
   </div>
 
-
-  <?php if ($wp_query->max_num_pages > 1) : ?>
+  <?php /*if ($wp_query->max_num_pages > 1) : ?>
     <nav class="post-nav">
       <ul class="pager">
         <li class="previous"><?php next_posts_link(__('&larr; Older posts', 'roots')); ?></li>
         <li class="next"><?php previous_posts_link(__('Newer posts &rarr;', 'roots')); ?></li>
       </ul>
     </nav>
-  <?php endif; ?>
+  <?php endif; */?>
 
   <?php get_template_part('templates/cms', 'reference'); ?>
 
