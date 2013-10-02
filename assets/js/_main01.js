63c62eeec142ea47974defc5b581cd12fca35a58
Leo Gono <hello@wpmeds.com>
2013-10-02 22:51:11 +0800

    updated scripts

--- assets/js/_main01.js
+++ assets/js/_main01.js
@@ -1,9 +1,8 @@
-jQuery(document).ready(function($) {
+jQuery(window).ready(function($) {
 	$('input, textarea').placeholder();
 
+	// var $videoFrame = $('#cms-intro-box .cms-video iframe');
+	// var $videoFrameSrc = $('#cms-intro-box .cms-video iframe').attr('src');
+	// $($videoFrame).attr('src', $videoFrameSrc+'?videoFoam=true')
 
-	// var player = document.getElementById('e-n8cbenSVI');
-	// $('#videoModal').on('hide', function () {
-	// 	player.stopVideo();
-	// });
 });
