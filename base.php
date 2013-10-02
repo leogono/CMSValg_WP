13302e2eb35d11266563bd379cbdb9d20666081b
Leo Gono <hello@wpmeds.com>
2013-10-02 17:12:02 +0800

    remove unnecessary commented out markup

--- base.php
+++ base.php
@@ -32,20 +32,6 @@
           <?php
         } ?>
 
-        <!-- <ul class="clearfix">
-          <li><a href="#" class="bcrumbs url">Forside</a></li>
-          <li class="cms-drop current">
-            <i class="icon-caret-right"></i>
-            <a href="#" class="bcrumbs url dropdown-toggle" data-toggle="dropdown">Umbraco<i class="icon-caret-down"></i></a>
-            <ul class="dropdown-menu">
-              <li><a href="#">Action</a></li>
-              <li><a href="#">Another action</a></li>
-              <li><a href="#">Something else here</a></li>
-              <li class="divider"></li>
-              <li><a href="#">Separated link</a></li>
-            </ul>
-          </li>
-        </ul> -->
       </div>
     </div>
     <?php
@@ -54,20 +40,6 @@
   <main id="main">
     <?php include roots_template_path(); ?>
   </main>
-  <?php /*
-  <div class="wrap container" role="document">
-    <div class="content row">
-      <div class="main <?php echo roots_main_class(); ?>" role="main">
-        <?php include roots_template_path(); ?>
-      </div><!-- /.main -->
-      <?php if (roots_display_sidebar()) : ?>
-        <aside class="sidebar <?php echo roots_sidebar_class(); ?>" role="complementary">
-          <?php include roots_sidebar_path(); ?>
-        </aside><!-- /.sidebar -->
-      <?php endif; ?>
-    </div><!-- /.content -->
-  </div><!-- /.wrap -->
-  */ ?>
 
   <?php get_template_part('templates/footer'); ?>
 
