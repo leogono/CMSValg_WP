eb6e2afb4b95e331c8ec0323efa77ec07a263eb0
Leo Gono <hello@wpmeds.com>
2013-10-02 23:05:12 +0800

    added simple page template style

--- page.php
+++ page.php
@@ -1,2 +1,6 @@
-<?php get_template_part('templates/page', 'header'); ?>
-<?php get_template_part('templates/content', 'page'); ?>
+<div class="container page">
+	<div class="page-inner">
+		<?php get_template_part('templates/page', 'header'); ?>
+		<?php get_template_part('templates/content', 'page'); ?>
+	</div>	
+</div>
