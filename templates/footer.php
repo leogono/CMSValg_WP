23feb05c2f1601792a06a676f6c317facb61ce33
Leo Gono <hello@wpmeds.com>
2013-10-05 02:47:29 +0800

    integration footer_text option

--- templates/footer.php
+++ templates/footer.php
@@ -1,20 +1,17 @@
-<?php /*
-<footer class="content-info container" role="contentinfo">
-  <div class="row">
-    <div class="col-lg-12">
-      <?php dynamic_sidebar('sidebar-footer'); ?>
-      <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
-    </div>
-  </div>
-</footer>
-*/ ?>
+<?php $footer_text = get_field('footer_text', 'option'); ?>
+
 <footer id="site-info">
   <div class="container">
     <div class="row">
       <div class="col-sm-6">
         <div class="footer-info">
           <a href="#" class="novicel-logo spr">Novicell</a>
-          <p>CMSValg er tænkt, udviklet og drevet af Novicell. Vi bygger intelligente webløsninger og er 35 udviklere, 4 designere, 5 online marketing eksperter.</p>
+          <?php if($footer_text) {
+            echo $footer_text;
+          } else {
+            echo '<p>CMSValg er tænkt, udviklet og drevet af Novicell. Vi bygger intelligente webløsninger og er 35 udviklere, 4 designere, 5 online marketing eksperter.</p>';
+          } ?>
+          
         </div>
       </div>
       <div class="col-sm-6">
