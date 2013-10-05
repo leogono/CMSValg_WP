29786a181d8893333c862439d1382d77622248f4
Leo Gono <hello@wpmeds.com>
2013-10-05 19:13:50 +0800

    corrected comment count on  content.php

--- templates/content.php
+++ templates/content.php
@@ -22,6 +22,6 @@
   </a>
   <h1 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
   <div class="comment-count-wrap">
-    <a href="#" class="comment-count"><i class="icon-comment"></i> 5</a>
+    <a href="<?php the_permalink(); ?>#comments-tag" class="comment-count"><i class="icon-comment"></i> <?php comments_number( '0', '1', '%' ); ?></a>
   </div>
 </article>
