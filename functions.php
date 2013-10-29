ab8b8e68bf3ef75ccabbae08e700cb781cd57f0f
Leo Gono <hello@wpmeds.com>
2013-10-29 16:54:28 +0800

    disabled relative urls

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@ require_once locate_template('/lib/nav.php');             // Custom nav modifica
 require_once locate_template('/lib/gallery.php');         // Custom [gallery] modifications
 require_once locate_template('/lib/comments.php');        // Custom comments modifications
 require_once locate_template('/lib/rewrites.php');        // URL rewriting for assets
-require_once locate_template('/lib/relative-urls.php');   // Root relative URLs
+// require_once locate_template('/lib/relative-urls.php');   // Root relative URLs
 require_once locate_template('/lib/widgets.php');         // Sidebars and widgets
 require_once locate_template('/lib/scripts.php');         // Scripts and stylesheets
 require_once locate_template('/lib/custom.php');          // Custom functions
